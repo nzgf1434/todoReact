bcded8fcfb2d71a1238a0710fa02af94aea33d4d
Malyshev Denis <a2l.saratov@gmail.com>
2018-12-25 15:00:26 +0400

    Add input for new task

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import './item-add-form.css';
 export default class ItemAddForm extends React.Component{
-    
+    state = {
+        label: ""
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onAdded(this.state.label);
+    };
+
+    onLabelChange = (e) => {
+        this.setState({label: e.target.value})
+    };
+
+
     render(){
         let {onAdded} = this.props;
         return (
-            <div className="item-add-form">
-                <button className="btn btn-outline-secondary"
-                onClick={() => onAdded("new task")}
-                >
+            <form className="item-add-form d-flex"
+                onSubmit={this.onSubmit}
+            >
+                <input type="text"
+                    className="form-control"
+                    onChange={this.onLabelChange}
+                    placeholder="Enter new task's name"                
+                />
+                <button className="btn btn-outline-secondary">
                     Add Item
                 </button>
-            </div>
+            </form>
         )
     }
 }
